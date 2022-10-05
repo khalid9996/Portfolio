0efb545634051ce5d98b749326120498962e1d64
khalid9996 <khankhalid1743@gmail.com>
2022-10-05 12:58:46 +0530

    added img

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import './home.css'
 import Btn from './Btn'
-import ME from '../../assets/ME.jpg'
+import ME from '../../assets/ME2.jpg'
 import HomeSocial from './HomeSocial'
 import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div className='view'>
-    <header>
-      <div className="container header__container">
-      <h5 className='h5__color'>Bonjour je suis</h5>
-        <h5 className='h5__color'>Hello I'am</h5>
-        <h1>Mohd Khalid Khan</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
-        <Btn/>
-        <HomeSocial/>
-        <div className="me">
-          <img src={ME} alt='me'></img>
+    <div className="view">
+      <header>
+        <div className="container header__container">
+          <h5 className="h5__color">Bonjour je suis</h5>
+          <h5 className="h5__color">Hello I'am</h5>
+          <h1>Mohd Khalid Khan</h1>
+          <h5 className="text-light">Fullstack Developer</h5>
+          <Btn />
+          <HomeSocial />
+          <div className="me">
+            <img src={ME} alt="me"></img>
+          </div>
+          <span className="space"></span>
+          <Link to="/contact" className="scroll__down">
+            Contact ~
+          </Link>
         </div>
-        <span className='space'></span>
-        <Link to='/contact' className='scroll__down'>Contact ~</Link>
-
-      </div>
-    </header>
+      </header>
     </div>
   )
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,6 +5,7 @@ function Services() {
   return (
     <>
     <section className=''>Services</section>
+    <iframe className='threed' src='https://my.spline.design/hands3duicopy-1f2cbfd5f97365a8f92e4d2bbdf11a4b/' frameborder='0' width='100%' height='100%'></iframe>
     </>
   )
 }
